216e1b2be71e09e4f30aea1db7a0333c5e71e676
BianPuelo <bianca@puelotech.com>
2022-05-25 01:42:19 -0300

    script terminado

--- script.sh
+++ script.sh
@@ -1,9 +1,7 @@
 echo "a) y c)"
 sed 's/\./.\n/g' breve_historia.txt | sed 's/^ //g' > breve_historia_2.txt
 cat breve_historia_2.txt
-## s marca sustitución
-## g sirve para que siga ejecutándose y encuentre todos los puntos a pesar de haber encontrado el primero
-## Además, borro los espacios de adelante sobrantes
+## Borro los espacios de adelante sobrantes
 
 echo ""
 echo "b) y c)"
@@ -12,7 +10,7 @@ sed -i '/^$/d' breve_historia_2.txt && cat breve_historia_2.txt
 
 echo ""
 echo "d)"
-grep -i 'guerra' breve_historia.txt
+grep --color -i 'guerra' breve_historia.txt
 
 echo ""
 echo "e)"
@@ -30,3 +28,15 @@ echo "La cantidad de veces de oraciones con las palabras Sarmiento y/o Rosas es:
 
 echo ""
 echo "h)"
+echo "$(sed 's/\./.\n/g' breve_historia.txt | sed 's/^ //g' | egrep '18[0-9]{2}|1900')"
+
+echo ""
+echo "i)"
+echo "$(sed 's/^[a-zA-Z]*\b//g' breve_historia.txt)" 
+
+echo ""
+echo "j)"
+echo "Cantidad de archivos .txt en el directorio actual: $(ls | grep -c .txt)"
+echo "Si desea ver la cantidad de otro directorio, ingreselo: "
+read directorio
+echo "Cantidad de archivos .txt en el directorio $directorio es: $(ls $directorio | grep -c .txt)"
